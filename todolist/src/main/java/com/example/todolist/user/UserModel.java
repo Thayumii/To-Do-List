254273da1cb1a5a1b494d9728b659f7b4a93ed22
Thalia <taliafreitas8@hotmail.com>
2024-11-19 20:36:54 -0300

    first commit

---
+++ todolist/src/main/java/com/example/todolist/user/UserModel.java
@@ -0,0 +1,12 @@
+package com.example.todolist.user;
+
+import lombok.Data;
+
+@Data
+public class UserModel {
+    
+    private String username;
+    private String name;
+    private String password;
+
+}
