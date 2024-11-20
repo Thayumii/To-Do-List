3eabd0261cb17a0eca51ce1fa7005eb76d36b64f
Thalia <taliafreitas8@hotmail.com>
2024-11-19 21:07:12 -0300

    created database

--- todolist/src/main/java/com/example/todolist/user/UserController.java
+++ todolist/src/main/java/com/example/todolist/user/UserController.java
@@ -3,6 +3,7 @@ package com.example.todolist.user;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 
 
@@ -10,9 +11,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 @RequestMapping("/users")
 public class UserController {
 
-@PostMapping("/")
+    @Autowired
+    private IUserRepository userRepository;
 
-    public void create(@RequestBody UserModel userModel) {
-        System.out.println(userModel.getUsername());
+    @PostMapping("/")
+    public UserModel create(@RequestBody UserModel userModel) {
+        var userCreated = this.userRepository.save(userModel);
+        return userCreated;
     }
 }
